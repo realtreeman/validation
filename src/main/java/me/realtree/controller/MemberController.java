06a6a7a80f596e922a15c5803d7489e0ff455ea1
realtreeman <zzan9150@gmail.com>
2022-06-14 16:36:06 +0900

    유효성검사2

---
+++ src/main/java/me/realtree/controller/MemberController.java
@@ -0,0 +1,36 @@
+package me.realtree.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.realtree.model.Member;
+import me.realtree.validation.MemberValidator;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@GetMapping("/success")
+	public String success() {
+		return "member/success";
+	}
+	
+	@GetMapping("/register")
+	public String registerForm(Member member) {
+		return "member/register";
+	}
+	@PostMapping("/register")
+	public String register(@Valid Member member, Errors errors) {
+		new MemberValidator().validate(member, errors);
+		if(errors.hasErrors()) {
+			return "member/register";
+		}
+		return "redirect::success";
+	}
+}
---
+++ src/main/java/me/realtree/validation/MemberValidator.java
@@ -0,0 +1,26 @@
+package me.realtree.validation;
+
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import me.realtree.model.Member;
+
+public class MemberValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Member.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Member member = (Member) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required");
+		
+	}
+
+}
